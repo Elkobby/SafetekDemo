8ccaf38f69d2d3254156f673a23c9bec3d8499c9
Eldad Nutakor <Eldad Nutakor>
2017-01-27 01:20:38 +0000

    added two new files

---
+++ groupdash.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="css/style.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body>
+	<div class="groupdashHeader">
+		<p>MeetPoints</p>
+	</div>
+		<div class="groupdashBody">
+			<p>Eng. Gate</p>
+		</div>
+	</body>
+</html>
